6d2e42b1c6c9fa5807fbe1e1ab19f44e68e4207f
ohenriquesouza <henriquesouzafagundes2003@gmail.com>
2023-01-18 22:17:26 -0300

    new recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,11 +12,15 @@ export class RecipeListComponent {
 
  recipes: Recipe[] = [
     new Recipe('BigMac', 'O queridinho de todos', 
-    'https://www.farofamagazine.com.br/wp-content/uploads/2022/05/big.jpg'),
+    'https://www.farofamagazine.com.br/wp-content/uploads/2022/05/big.jpg', '503 kcal'),
     new Recipe('Big Tasty', 'Pra quando bater a Big Fome', 
-    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kzXKz2eL/200/200/original?country=br'),
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kzXKz2eL/200/200/original?country=br', '944 kcal'),
     new Recipe('Chedar McMelt', 'O brasileirinho mais aclamado', 
-    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXu0yRK/200/200/original?country=br')
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXu0yRK/200/200/original?country=br', '474 kcal'),
+    new Recipe('Quarteirão com Queijo', 'Uma explosão de queijo', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXDlz7u/200/200/original?country=br', '523 kcal'),
+    new Recipe('McNífico Bacon', 'Não precisa nem dizer nada', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXEsSfl/200/200/original?country=br', '571 kcal')
   ];
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,10 +2,12 @@ export class Recipe {
     public name: string;
     public description: string;
     public imagesPath: string;
+    public kcal: string;
 
-    constructor(name: string, description: string, imagesPath: string){
+    constructor(name: string, description: string, imagesPath: string, kcal: string){
         this.name = name;
         this.description = description;
         this.imagesPath = imagesPath;
+        this.kcal = kcal;
     }
 }
