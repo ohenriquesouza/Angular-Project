70c2f86a7b8818d891f77cffcc30157319281782
ohenriquesouza <henriquesouzafagundes2003@gmail.com>
2023-01-18 22:41:26 -0300

    desserts added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -20,7 +20,17 @@ export class RecipeListComponent {
     new Recipe('Quarteirão com Queijo', 'Uma explosão de queijo', 
     'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXDlz7u/200/200/original?country=br', '523 kcal'),
     new Recipe('McNífico Bacon', 'Não precisa nem dizer nada', 
-    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXEsSfl/200/200/original?country=br', '571 kcal')
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kfXEsSfl/200/200/original?country=br', '571 kcal'),
+    new Recipe('Sundae Chocolate', 'Adicionar desc', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kpXuLeTy/200/200/original?country=br', '313 kcal'),
+    new Recipe('Top Sundae Caramelo', 'Adicionar desc', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kpXFEpzl/200/200/original?country=br', '411 kcal'),
+    new Recipe('Casquinha Mista', 'Adicionar desc', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kpX0NLJ6/200/200/original?country=br', '183 kcal'),
+    new Recipe('Torta de Maça', 'Adicionar desc', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$qLX2gXaY/200/200/original?country=br', '251 kcal'),
+    new Recipe('McShake OvoMaltine', 'Adicionar desc', 
+    'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kJX0TX33/200/200/original?country=br', '548 kcal')
   ];
 
   onRecipeSelected(recipe: Recipe){
